2afbb0c5dda2a1f3b492435ae6e0a10376657cf6
echo <miyunniecho@gmail.com>
2021-03-11 23:24:49 -0800

    create columns for models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    produce_name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    }, 
+    price: {
+      type: DataTypes.DECIMAL, 
+      allowNull: false, 
+      validate: {
+        isInt: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+// set default value of 10 and validates the value is numeric 
+      validate:{
+        equals: 10, 
+        isNumeric: true,  
+      }
+    }, 
+    category_id: {
+      type: DataTypes.INTEGER, 
+      references: {
+        model: 'category', 
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
